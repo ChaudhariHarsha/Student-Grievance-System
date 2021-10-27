152da698e354e2c2add9faca95b126c5ff7d51b3
Harshal Hanumant Chaudhari <53361439+ChaudhariHarsha@users.noreply.github.com>
2021-10-27 14:52:30 +0530

    Add files via upload

---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              Copyright © 2020. Directorate of Educational Department. All Rights are reserved!
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
